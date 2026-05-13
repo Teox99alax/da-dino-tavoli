97e4e3fbc02a4877394cd3fff8dc5e2b5157bc2f
Teox99alax <teox99@gmail.com>
2026-05-13 17:25:59 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -5,5 +5,10 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(
   supabaseUrl,
-  supabaseAnonKey
+  supabaseAnonKey,
+  {
+    auth: {
+      persistSession: false,
+    },
+  }
 );
